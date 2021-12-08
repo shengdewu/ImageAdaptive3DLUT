eb3e265321ea14184ef414f2b9922bb8a9896897
baoyong <baoyong@super.com>
2021-12-08 15:22:21 +0800

    update

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from schedule.AdaptiveScheduler import AdaptiveScheduler
 if __name__ == '__main__':
     AdaptiveScheduler().schedule()
 
-    # from dataloader.dataset_xintu import process_label
-    # process_label()
+    # from dataloader.dataset_xintu import create_train_label
+    # create_train_label('/mnt/data/data.set/xintu.data/pre.image.enhance.540')
+
