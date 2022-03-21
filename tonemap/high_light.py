5c1f7044d18902d3d4ef1058c4523efa397db993
baoyong <baoyong@super.com>
2022-03-21 14:28:30 +0800

    update

--- tonemap/high_light.py
+++ tonemap/high_light.py
@@ -9,12 +9,13 @@ def tone_high_light(image:torch.Tensor, light=50.0):
     :return:
     '''
     luma = rgb2luma_bt601(image)
+    # 确定高光区域
     thresh = luma * luma
+    # 取平均值做阈值
     t = torch.mean(thresh)
-
     mask = torch.where(thresh >= t, 255, 0).short()
 
-    # 高光平滑
+    # 边缘平滑过渡
     max_val = 4
     bright = light / 100.0 / max_val
     mid = 1.0 + max_val * bright
@@ -25,6 +26,7 @@ def tone_high_light(image:torch.Tensor, light=50.0):
     bright = torch.zeros(size=(1, 1), dtype=torch.float32).to(mask.device) + bright
     bright_rate = torch.where(mask == 255, bright, bright_tmp)
 
+    # 获取结果图
     tmp = torch.pow(image, 1.0/mid_rate) * (1.0 / (1 - bright_rate))
     return torch.clip(tmp, 0.0, 1.0)
 
