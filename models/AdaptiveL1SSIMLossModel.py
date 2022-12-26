f0d10890dbf57b5c52b919a6be9c0760c6498289
baoyong <you@example.com>
2022-12-26 17:51:26 +0800

    update

--- models/AdaptiveL1SSIMLossModel.py
+++ models/AdaptiveL1SSIMLossModel.py
@@ -12,8 +12,8 @@ class AdaptiveL1SSIMLossModel(AdaptiveBaseModel):
         super(AdaptiveL1SSIMLossModel, self).__init__(cfg)
         self.ssim_loss = engine_ssim.SSIM()
         self.criterion_pixelwise = torch.nn.L1Loss().to(self.device)
-        self.criterion_perceptual = PerceptualLoss(cfg.MODEL.VGG.VGG_LAYER, device=self.device, path=cfg.MODEL.VGG.VGG_PATH)
-        self.cos_loss = torch.nn.CosineSimilarity(dim=1)
+        # self.criterion_perceptual = PerceptualLoss(cfg.MODEL.VGG.VGG_LAYER, device=self.device, path=cfg.MODEL.VGG.VGG_PATH)
+        # self.cos_loss = torch.nn.CosineSimilarity(dim=1)
 
         self.lambda_ssim = cfg.SOLVER.LAMBDA_SSIM
         self.lambda_perceptual = cfg.SOLVER.LAMBDA_PERCEPTUAL
@@ -38,12 +38,16 @@ class AdaptiveL1SSIMLossModel(AdaptiveBaseModel):
         tv_cons = sum(tv1) + tv0
         mn_cons = sum(mn1) + mn0
 
-        loss_perceptual = self.criterion_perceptual(fake_B, real_B)
-        cos_loss = 1 - torch.mean(self.cos_loss(fake_B, real_B))
+        # loss_perceptual = self.criterion_perceptual(fake_B, real_B)
+        # cos_loss = 1 - torch.mean(self.cos_loss(fake_B, real_B))
 
-        loss = self.lambda_pixel * loss_pixel + self.lambda_perceptual * loss_perceptual + \
+        # loss = self.lambda_pixel * loss_pixel + self.lambda_perceptual * loss_perceptual + \
+        #        self.lambda_smooth * tv_cons + self.lambda_class_smooth * weights_norm + \
+        #        self.lambda_monotonicity * mn_cons + self.lambda_ssim * ssim_loss + self.lambda_cos * cos_loss
+
+        loss = self.lambda_pixel * loss_pixel + self.lambda_ssim * ssim_loss + \
                self.lambda_smooth * tv_cons + self.lambda_class_smooth * weights_norm + \
-               self.lambda_monotonicity * mn_cons + self.lambda_ssim * ssim_loss + self.lambda_cos * cos_loss
+               self.lambda_monotonicity * mn_cons
 
         psnr_avg = 10 * math.log10(1 / loss_pixel.item())
 
@@ -59,8 +63,8 @@ class AdaptiveL1SSIMLossModel(AdaptiveBaseModel):
                 'total_loss': loss.item(),
                 'loss_pixel': loss_pixel.item(),
                 'ssim_loss': ssim_loss.item(),
-                'perceptual': loss_perceptual.item(),
-                'cos_loss': cos_loss.item(),
+                # 'perceptual': loss_perceptual.item(),
+                # 'cos_loss': cos_loss.item(),
                 'mn_cons': mn_cons.item(),
                 'tv_cons': tv_cons.item(),
                 'weights_norm': weights_norm.item(),
